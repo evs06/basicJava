19595a9908b8c09cbd963db971a38ed6a488dcfd
Emmanuel <evs061191@gmail.com>
2020-04-24 19:54:45 -0500

    Faltó un ejemplo al 24-04-20

---
+++ src/iLoops/AgregarAmigo.java
@@ -0,0 +1,29 @@
+package iLoops;
+
+public class AgregarAmigo {
+
+    public static void main(String[] args) {
+        navegar("https://facebook.com");
+        login("user", "password");
+        buscar("Juan Penas");
+        agregar("Queretaro");
+    }
+
+    public static void navegar(String url) {
+
+    }
+
+    public static void login(String usuario, String password) {
+
+    }
+
+    public static void buscar(String nombre){
+
+
+    }
+
+    public static void agregar(String info){
+
+
+    }
+}
